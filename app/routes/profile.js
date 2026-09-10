b24b3bd5300f66e90b7e07e81e30209723709fd2
CassandraGoose <21070575+CassandraGoose@users.noreply.github.com>
2026-09-10 15:40:51 -0600

    update regex for bank routing

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -46,17 +46,9 @@ function ProfileHandler(db) {
             bankRouting,
         } = req.body;
 
-        // Fix for Section: ReDoS attack
-        // The following regexPattern that is used to validate the bankRouting number is insecure and vulnerable to
-        // catastrophic backtracking which means that specific type of input may cause it to consume all CPU resources
-        // with an exponential time until it completes
-        // --
-        // The Fix: Instead of using greedy quantifiers the same regex will work if we omit the second quantifier +
-        // const regexPattern = /([0-9]+)\#/;
-        const regexPattern = /([0-9]+)+\#/;
-        // Allow only numbers with a suffix of the letter #, for example: 'XXXXXX#'
+        const regexPattern = /^[0-9]+#$/;
         const testComplyWithRequirements = regexPattern.test(bankRouting);
-        // if the regex test fails we do not allow saving
+
         if (testComplyWithRequirements !== true) {
             const firstNameSafeString = firstName;
             return res.render("profile", {
@@ -85,17 +77,14 @@ function ProfileHandler(db) {
             address,
             bankAcc,
             bankRouting,
-            (err, user) => {
+            (err) => {
                 if (err) return next(err);
 
-                // WARN: Applying any sting specific methods here w/o checking type of inputs could lead to DoS by HPP
-                // firstName = firstName.trim();
-                user.updateSuccess = true;
-                user.userId = userId;
-
-                return res.render("profile", {
-                    ...user,
-                    environmentalScripts,
+                profile.getByUserId(parseInt(userId), (err, decryptedUser) => {
+                    if (err) return next(err);
+                    decryptedUser.updatedSuccess = true;
+                    decryptedUser.userId = userId;
+                    res.render("profile", { ...decryptedUser, environmentalScripts });
                 });
             },
         );
